b0d799a85629cfbe2fc8c4c3cdf576acbd011e91
LEVIS RABAH <levisrabah@gmail.com>
2024-03-22 15:05:32 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependcat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function destructivelyPrependCat(){
+    return [name, ...cats];
+}
+function appendCat(name){
+    return [...cats, name];
+}
+function removeFirstCat(){
+    return cats.slice[1];
+}
+function prependCat(name){
+    return [...cats, name]
+}
+function removeLastCat(){
+    return cats.slice(0, cats.length -1);
+}
+function destructivelyPrependCat (name) {
+    cats.unshift(name);
+  }
+  function prependCat (name) {
+    return [name, ...cats];
+  }
+  function removeFirstCat () {
+    return cats.slice(1);
+  }
